02e0b511f690c5a88b2e84ab4aad768eff778aa0
Amit Singh Rana <ranaa73000@gmail.com>
2022-01-26 13:28:11 +0530

    Getting new Product data from 'POST' request

--- api/routes/products.js
+++ api/routes/products.js
@@ -8,8 +8,15 @@ router.get("/", (req, res, next) => {
 });
 
 router.post("/", (req, res, next) => {
+  // Creating the new product object
+  const product = {
+    name: req.body.name,
+    price: req.body.price,
+  };
+
   res.status(200).json({
     message: "POST /products",
+    createdProduct: product,
   });
 });
 
