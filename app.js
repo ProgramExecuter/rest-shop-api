8467c6999522b0de1ae36aa4221dd89b379a6f0a
Amit Singh Rana <ranaa73000@gmail.com>
2022-01-26 13:12:00 +0530

    Basic Error Handling

--- app.js
+++ app.js
@@ -1,10 +1,31 @@
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
 
 const productRoutes = require("./api/routes/products");
 const orderRoutes = require("./api/routes/orders");
 
+app.use(morgan("dev"));
+
+// Routes for handling requests
 app.use("/products", productRoutes);
 app.use("/orders", orderRoutes);
+
+// If this is reached then no file in above lines was able to handle this request
+app.use((req, res, next) => {
+  const error = new Error("Not Found");
+  error.status = 404;
+  next(error);
+});
+
+// This handles all errors thrown by some operations
+app.use((error, req, res, next) => {
+  res.status(error.status);
+  res.json({
+    error: {
+      message: error.message,
+    },
+  });
+});
 
 module.exports = app;
