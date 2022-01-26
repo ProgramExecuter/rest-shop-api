76979ab04a4a02bff04ed66770f68a32394bf121
Amit Singh Rana <ranaa73000@gmail.com>
2022-01-26 15:19:24 +0530

    Added CORS handling

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+// CORS Handling
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
