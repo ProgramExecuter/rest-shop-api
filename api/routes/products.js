5746b326ad5f88160b721517b6fdd8510a7719cf
Amit Singh Rana <ranaa73000@gmail.com>
2022-01-26 17:36:29 +0530

    Error Handling for GET /products/:id

--- api/routes/products.js
+++ api/routes/products.js
@@ -47,7 +47,13 @@ router.get("/:productId", (req, res, next) => {
     .exec()
     .then((doc) => {
       console.log(doc);
-      res.status(200).json(doc);
+
+      // Check if the product exist
+      if (doc) {
+        res.status(200).json(doc);
+      } else {
+        res.status(404).json({ message: "No Product Found" });
+      }
     })
     .catch((err) => {
       console.log(err);
