41b2e1ad4157fc3dc14f6187afd53863dfe9b5ec
Amit Singh Rana <ranaa73000@gmail.com>
2022-01-26 17:15:10 +0530

    New Product Adding available

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+
+const Product = require("../models/product");
 
 router.get("/", (req, res, next) => {
   res.status(200).json({
@@ -9,12 +12,19 @@ router.get("/", (req, res, next) => {
 
 router.post("/", (req, res, next) => {
   // Creating the new product object
-  const product = {
+  const product = new Product({
+    _id: new mongoose.Types.ObjectId(),
     name: req.body.name,
     price: req.body.price,
-  };
+  });
 
-  res.status(200).json({
+  // Store this in DB
+  product
+    .save()
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err));
+
+  res.status(201).json({
     message: "POST /products",
     createdProduct: product,
   });
