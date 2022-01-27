e759b9adb3b3797b94b5b22138bd81ac460ab0e0
Amit Singh Rana <ranaa73000@gmail.com>
2022-01-27 17:40:02 +0530

    Getting all orders, GET /products implemented

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -4,12 +4,39 @@ const mongoose = require("mongoose");
 
 const Order = require("../models/order");
 
+//
+// Get all Orders
 router.get("/", (req, res, next) => {
-  res.status(200).json({
-    message: "GET /orders",
-  });
+  Order.find()
+    .select("product _id quantity")
+    .exec()
+    .then((docs) => {
+      // Structure and data of all products
+      const orders = docs.map((doc) => {
+        return {
+          _id: doc._id,
+          product: doc.product,
+          quantity: doc.quantity,
+          request: {
+            type: "GET",
+            url: `localhost:3000/orders/${doc._id}`,
+          },
+        };
+      });
+
+      // Structure of response
+      res.status(200).json({
+        count: docs.length,
+        orders: orders,
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err });
+    });
 });
 
+//
+// Add a new Order
 router.post("/", (req, res, next) => {
   // Creating the new order object
   const order = new Order({
