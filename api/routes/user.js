88d4185598eefe1fe9ead37092edb72a10b5b232
Amit Singh Rana <ranaa73000@gmail.com>
2022-01-28 21:12:58 +0530

    'Cannot re-register same email' feature added

--- api/routes/user.js
+++ api/routes/user.js
@@ -8,32 +8,47 @@ const User = require("../models/user");
 //
 // Create New User(Sign Up)
 router.post("/signup", (req, res, next) => {
-  // Hash the password
-  bcrypt.hash(req.body.password, 10, (err, hash) => {
-    if (err) {
+  // Check if this email is already registered
+  User.find({ email: req.body.email })
+    .then((user) => {
+      if (user.length >= 1) {
+        // this 'email' is registered
+        return res.status(422).json({ message: "Email already registered" });
+      } else {
+        // If this 'email' isn't registered
+
+        // Hash the password
+        bcrypt.hash(req.body.password, 10, (err, hash) => {
+          if (err) {
+            console.log(err);
+            res.status(500).json({ error: err });
+          } else {
+            // If we were able to hash the password
+            // then create new User
+            const user = new User({
+              _id: new mongoose.Types.ObjectId(),
+              email: req.body.email,
+              password: hash,
+            });
+
+            user
+              .save()
+              .then((result) => {
+                console.log(result);
+                res.status(201).json({ message: "User Created" });
+              })
+              .catch((err) => {
+                console.log(err);
+                res.status(500).json({ error: err });
+              });
+          }
+        });
+      }
+    })
+    .catch((err) => {
       console.log(err);
       res.status(500).json({ error: err });
-    } else {
-      // If we were able to hash the password
-      // then create new User
-      const user = new User({
-        _id: new mongoose.Types.ObjectId(),
-        email: req.body.email,
-        password: hash,
-      });
-
-      user
-        .save()
-        .then((result) => {
-          console.log(result);
-          res.status(201).json({ message: "User Created" });
-        })
-        .catch((err) => {
-          console.log(err);
-          res.status(500).json({ error: err });
-        });
-    }
-  });
+    });
 });
 
 module.exports = router;
