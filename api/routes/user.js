f9b95da780ecdf860c5a876a57cf76d569608dcd
Amit Singh Rana <ranaa73000@gmail.com>
2022-01-29 18:16:39 +0530

    'User Deletion' feature added

--- api/routes/user.js
+++ api/routes/user.js
@@ -51,4 +51,16 @@ router.post("/signup", (req, res, next) => {
     });
 });
 
+router.delete("/:userId", (req, res, next) => {
+  User.findByIdAndDelete(req.params.userId)
+    .exec()
+    .then((result) => {
+      res.status(200).json({ message: "User Deleted" });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ error: err });
+    });
+});
+
 module.exports = router;
