5024a5b79a2d21990ea7c6a89548b74a8501ddf2
Amit Singh Rana <ranaa73000@gmail.com>
2022-01-29 19:14:38 +0530

    Added 'Getting login successfull token'

--- api/routes/user.js
+++ api/routes/user.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const User = require("../models/user");
 
@@ -71,7 +72,17 @@ router.post("/login", (req, res, next) => {
 
         // If the password matches
         if (result) {
-          return res.status(200).json({ message: "Auth Success" });
+          const token = jwt.sign(
+            {
+              email: user.email,
+              userId: user._id,
+            },
+            process.env.JWT_KEY,
+            {
+              expiresIn: "1h",
+            }
+          );
+          return res.status(200).json({ message: "Auth Success", token });
         } else {
           return res.status(401).json({ message: "Auth Failed" });
         }
