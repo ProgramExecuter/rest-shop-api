ff8567aebce25d255bbe920d29898dcb3b1d6a24
Amit Singh Rana <ranaa73000@gmail.com>
2022-01-30 20:06:36 +0530

    Moved userLogin to 'controller'

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -53,6 +53,50 @@ const signUp = (req, res, next) => {
     });
 };
 
+const userLogin = (req, res, next) => {
+  // Find if a user is registered with this email
+  User.findOne({ email: req.body.email })
+    .exec()
+    .then((user) => {
+      // This email isn't registered
+      if (user == null) {
+        return res.status(401).json({ message: "Auth Failed" });
+      }
+
+      // Email is registered
+
+      // Compare the password with hashed password in DB
+      bcrypt.compare(req.body.password, user.password, (err, result) => {
+        // If any error occured, return
+        if (err) {
+          return res.status(401).json({ message: "Auth Failed" });
+        }
+
+        // If the password matches
+        if (result) {
+          const token = jwt.sign(
+            {
+              email: user.email,
+              userId: user._id,
+            },
+            process.env.JWT_KEY,
+            // Expire this token after '1 Hour'
+            { expiresIn: "1h" }
+          );
+          return res.status(200).json({ message: "Auth Success", token });
+        } else {
+          return res.status(401).json({ message: "Auth Failed" });
+        }
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+
+      res.status(500).json({ error: err });
+    });
+};
+
 module.exports = {
   signUp,
+  userLogin,
 };
--- api/routes/user.js
+++ api/routes/user.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 // Import Controllers
-const { signUp } = require("../controllers/user");
+const { signUp, userLogin } = require("../controllers/user");
 
 //
 // Create New User(Sign Up)
@@ -10,48 +10,7 @@ router.post("/signup", signUp);
 
 //
 // Sign In user
-router.post("/login", (req, res, next) => {
-  // Find if a user is registered with this email
-  User.findOne({ email: req.body.email })
-    .exec()
-    .then((user) => {
-      // This email isn't registered
-      if (user == null) {
-        return res.status(401).json({ message: "Auth Failed" });
-      }
-
-      // Email is registered
-
-      // Compare the password with hashed password in DB
-      bcrypt.compare(req.body.password, user.password, (err, result) => {
-        // If any error occured, return
-        if (err) {
-          return res.status(401).json({ message: "Auth Failed" });
-        }
-
-        // If the password matches
-        if (result) {
-          const token = jwt.sign(
-            {
-              email: user.email,
-              userId: user._id,
-            },
-            process.env.JWT_KEY,
-            // Expire this token after '1 Hour'
-            { expiresIn: "1h" }
-          );
-          return res.status(200).json({ message: "Auth Success", token });
-        } else {
-          return res.status(401).json({ message: "Auth Failed" });
-        }
-      });
-    })
-    .catch((err) => {
-      console.log(err);
-
-      res.status(500).json({ error: err });
-    });
-});
+router.post("/login", userLogin);
 
 //
 // Delete a particular User
